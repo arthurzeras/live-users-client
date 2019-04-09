7fb117558b845386946db08714f0b1f6d5fbb628
arthurzeras <arthurolvmorais@gmail.com>
2019-04-09 17:09:27 -0300

    Criando filtragem dos gráficos

--- src/pages/stats/Stats.vue
+++ src/pages/stats/Stats.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="most-accessed">
+    <header class="header-filter">
+      <label class="mb-0 pb-0">Filtrar:</label>
+      <select
+        v-model="filter"
+        @change="getData()"
+        class="form-control form-control-sm"
+      >
+        <option value="H">Hoje</option>
+        <option value="S">Essa Semana</option>
+        <option value="M">Esse Mês</option>
+        <option value="G">Geral</option>
+      </select>
+    </header>
+    <div class="text-right text-secondary" style="font-size: 10pt">
+      <span>* Os dados começaram a ser contabilizados no dia 01/04/2019</span>
+    </div>
+    <hr>
     <div class="row info-header">
       <div class="header-title col-12 col-lg-6">
         <h2>Cliente</h2>
@@ -51,6 +68,7 @@
 </template>
 
 <script>
+import moment from 'moment'
 import CardBox from './CardBox'
 import Top10 from './charts/Top10'
 import countBy from 'lodash.countby'
@@ -66,6 +84,7 @@ export default {
     Schedule
   },
   data: () => ({
+    filter: 'H',
     chartTop10: {
       CON: {},
       CLI: {}
@@ -87,13 +106,30 @@ export default {
           .keys(items)
           .map(item => items[item])
           .filter(item => item.env === 'prod')
+          .filter(item => this.filterPeriod(item))
 
         this.chartTop10.CLI = this.createTop10(items, 'cliente')
         this.chartTop10.CON = this.createTop10(items, 'contabilidade')
 
         this.frequencies.CLI = this.createFrequency(items, 'cliente')
         this.frequencies.CON = this.createFrequency(items, 'contabilidade')
+
+        items = null
       })
+    },
+    filterPeriod ({ createdAt }) {
+      const itemDate = moment(createdAt)
+
+      switch (this.filter) {
+        case 'H':
+          return itemDate.isSame(moment(), 'day')
+        case 'S':
+          return itemDate.isSame(moment(), 'week')
+        case 'M':
+          return itemDate.isSame(moment(), 'month')
+        case 'G':
+          return itemDate
+      }
     },
     createTop10 (items = [], type) {
       const ignored = ['/logout', '/painel/contador']
@@ -139,6 +175,15 @@ export default {
   overflow: hidden auto;
   height: calc(100vh - 71px);
   background-color: #E7E7E7;
+  .header-filter {
+    color: #333;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    select {
+      width: auto;
+    }
+  }
   .info-header {
     text-align: center;
     .header-title:first-child { color: #0066FE }
